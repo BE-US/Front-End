174b9a4d4bab66e1f0d18e2690ff3f78e06d9244
timotheekim10 <timotheekim10@gmail.com>
2023-09-25 15:38:32 +0900

    feat: 로그인 화면 로고 추가

--- BEUS/Login/LoginVC.swift
+++ BEUS/Login/LoginVC.swift
@@ -8,10 +8,34 @@
 import UIKit
 
 class LoginVC: UIViewController {
+    
+    let logo: UIImageView = UIImageView(image: UIImage(named: "Logo"))
+    
+    func makeSubView() {
+        view.addSubview(logo)
+    }
+    
+    func makeConstraint() {
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            logo.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 130),
+            logo.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 150),
+            logo.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -150),
+            logo.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            logo.heightAnchor.constraint(equalToConstant: 25)
+        ])
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        
+        makeSubView()
+        makeConstraint()
         navigationController?.isNavigationBarHidden = true
     }
+}
+
+extension LoginVC {
+    
 }
