039d3f5ed36ea791268c8381e0d0be7ed49738aa
timotheekim10 <timotheekim10@gmail.com>
2023-09-25 14:37:45 +0900

    feat: UIKit 기반 코드 베이스 프로젝트 세팅

---
+++ BEUS/Login/LoginVC.swift
@@ -0,0 +1,17 @@
+//
+//  LoginVC.swift
+//  BEUS
+//
+//  Created by 티모시 킴 on 2023/09/25.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        navigationController?.isNavigationBarHidden = true
+    }
+}
