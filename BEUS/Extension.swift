983423d4338d1783964028446224bf38ce4cfc16
timotheekim10 <timotheekim10@gmail.com>
2023-09-25 20:23:10 +0900

    feat: 로그인 화면 UI 요소 추가

--- BEUS/Extension.swift
+++ BEUS/Extension.swift
@@ -7,6 +7,34 @@
 
 import UIKit
 
+extension UIButton {
+    
+    func setUnderLine(){
+        guard let title = title(for: .normal) else { return }
+        let attributedString = NSMutableAttributedString(string: title)
+        attributedString.addAttribute(.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: title.count))
+        setAttributedTitle(attributedString, for: .normal)
+    }
+    
+}
+
+extension UIColor {
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+
+        let red = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgb & 0x0000FF) / 255.0
+
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+}
+
 extension UITextField {
     
     func addLeftPadding() {
--- BEUS/Login/LoginVC.swift
+++ BEUS/Login/LoginVC.swift
@@ -17,7 +17,7 @@ class LoginVC: UIViewController {
         emailTextField.backgroundColor = .systemGray6
         emailTextField.layer.cornerRadius = 20
         emailTextField.clearButtonMode = .whileEditing
-        let emailImage = UIImage(named: "mail")?.resizeImageTo(size: CGSize(width: 25, height: 25))
+        let emailImage = UIImage(named: "Mail")?.resizeImageTo(size: CGSize(width: 25, height: 25))
         emailTextField.addLeftImage(image: emailImage!)
         return emailTextField
     }()
@@ -28,21 +28,131 @@ class LoginVC: UIViewController {
         passwordTextField.backgroundColor = .systemGray6
         passwordTextField.layer.cornerRadius = 20
         passwordTextField.clearButtonMode = .whileEditing
-        let passwordImage = UIImage(named: "key")?.resizeImageTo(size: CGSize(width: 25, height: 25))
+        let passwordImage = UIImage(named: "Key")?.resizeImageTo(size: CGSize(width: 25, height: 25))
         passwordTextField.addLeftImage(image: passwordImage!)
         return passwordTextField
+    }()
+    
+    let loginButton : UIButton={
+        let loginButton = UIButton()
+        var title = AttributedString("로그인")
+        title.font = UIFont.boldSystemFont(ofSize: 20)
+        
+        var config = UIButton.Configuration.filled()
+        config.cornerStyle = .capsule
+        config.baseBackgroundColor = #colorLiteral(red: 0.5215686275, green: 0.8392156863, blue: 0.6509803922, alpha: 1)
+        config.attributedTitle = title
+        
+        loginButton.configuration = config
+        return loginButton
+    }()
+    
+    let findPasswordButton : UIButton = {
+        let findPasswordButton =  UIButton()
+        findPasswordButton.setTitle("비밀번호를 잊으셨나요?", for: .normal)
+        findPasswordButton.setTitleColor(UIColor(hex: "#333333"), for: .normal)
+        findPasswordButton.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        findPasswordButton.setUnderLine()
+        return findPasswordButton
+    }()
+    
+    let easyLoginView: UIView = {
+        let easyLoginView = UIView()
+        let label = UILabel()
+        label.text = "간편 로그인"
+        label.textColor = #colorLiteral(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+        label.font = UIFont.systemFont(ofSize: 15)
+        easyLoginView.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: easyLoginView.leadingAnchor),
+            label.trailingAnchor.constraint(equalTo: easyLoginView.trailingAnchor),
+            label.topAnchor.constraint(equalTo: easyLoginView.topAnchor),
+            label.bottomAnchor.constraint(equalTo: easyLoginView.bottomAnchor)
+        ])
+        return easyLoginView
+    }()
+    
+    let easyLoginLine1: UIView = {
+        let easyLoginLine1 = UIView()
+        easyLoginLine1.backgroundColor = .systemGray
+        return easyLoginLine1
+    }()
+    
+    let easyLoginLine2: UIView = {
+        let easyLoginLine2 = UIView()
+        easyLoginLine2.backgroundColor = .systemGray
+        return easyLoginLine2
+    }()
+    
+    let naverButton: UIButton = {
+        let naverButton = UIButton()
+        var title = AttributedString("네이버로 간편로그인하기")
+        title.font =  UIFont.systemFont(ofSize: 15, weight: .light)
+        var config = UIButton.Configuration.plain()
+        config.attributedTitle = title
+        config.background.strokeColor = #colorLiteral(red: 0.131085664, green: 0.8754685521, blue: 0.2015054226, alpha: 1)
+        config.baseForegroundColor = .black
+        config.cornerStyle = .capsule
+        let naverimage = UIImage(named: "Naver")!.resizeImageTo(size: CGSize(width: 20, height: 20))
+        config.image = naverimage
+        config.imagePadding = 10
+        naverButton.configuration = config
+        return naverButton
+    }()
+    
+    let signUpView: UIView = {
+        let signUpView = UIView()
+        let label = UILabel()
+        label.text = "아직 회원이 아니신가요?"
+        label.textColor = #colorLiteral(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+        label.font = UIFont.systemFont(ofSize: 15)
+        signUpView.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: signUpView.leadingAnchor),
+            label.trailingAnchor.constraint(equalTo: signUpView.trailingAnchor),
+            label.topAnchor.constraint(equalTo: signUpView.topAnchor),
+            label.bottomAnchor.constraint(equalTo: signUpView.bottomAnchor)
+        ])
+        return signUpView
+    }()
+    
+    let signUpButton: UIButton = {
+        let signUpButton = UIButton()
+        signUpButton.setTitle("가입하기", for: .normal)
+        signUpButton.setTitleColor(UIColor(hex: "#333333"), for: .normal)
+        signUpButton.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+        signUpButton.setUnderLine()
+        return signUpButton
     }()
     
     func makeSubView() {
         view.addSubview(logo)
         view.addSubview(emailTextField)
         view.addSubview(passwordTextField)
+        view.addSubview(loginButton)
+        view.addSubview(findPasswordButton)
+        view.addSubview(easyLoginView)
+        view.addSubview(easyLoginLine1)
+        view.addSubview(easyLoginLine2)
+        view.addSubview(naverButton)
+        view.addSubview(signUpView)
+        view.addSubview(signUpButton)
     }
     
     func makeConstraint() {
         logo.translatesAutoresizingMaskIntoConstraints = false
         emailTextField.translatesAutoresizingMaskIntoConstraints = false
         passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        findPasswordButton.translatesAutoresizingMaskIntoConstraints = false
+        easyLoginView.translatesAutoresizingMaskIntoConstraints = false
+        easyLoginLine1.translatesAutoresizingMaskIntoConstraints = false
+        easyLoginLine2.translatesAutoresizingMaskIntoConstraints = false
+        naverButton.translatesAutoresizingMaskIntoConstraints = false
+        signUpView.translatesAutoresizingMaskIntoConstraints = false
+        signUpButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             logo.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 130),
@@ -59,7 +169,42 @@ class LoginVC: UIViewController {
             passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 10),
             passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
             passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
-            passwordTextField.heightAnchor.constraint(equalToConstant: 45),
+            passwordTextField.heightAnchor.constraint(equalTo: emailTextField.heightAnchor),
+            
+            loginButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 20),
+            loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            loginButton.heightAnchor.constraint(equalTo: emailTextField.heightAnchor),
+            
+            findPasswordButton.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: 30),
+            findPasswordButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            findPasswordButton.widthAnchor.constraint(equalToConstant: 180),
+            findPasswordButton.heightAnchor.constraint(equalToConstant: 20),
+            
+            easyLoginView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            easyLoginView.topAnchor.constraint(equalTo: findPasswordButton.bottomAnchor, constant: 70),
+            
+            easyLoginLine1.topAnchor.constraint(equalTo: findPasswordButton.bottomAnchor, constant: 80),
+            easyLoginLine1.trailingAnchor.constraint(equalTo: easyLoginView.leadingAnchor, constant: -15),
+            easyLoginLine1.heightAnchor.constraint(equalToConstant: 1),
+            easyLoginLine1.widthAnchor.constraint(equalToConstant: 105),
+
+            easyLoginLine2.topAnchor.constraint(equalTo: findPasswordButton.bottomAnchor, constant: 80),
+            easyLoginLine2.leadingAnchor.constraint(equalTo: easyLoginView.trailingAnchor, constant: 15),
+            easyLoginLine2.heightAnchor.constraint(equalToConstant: 1),
+            easyLoginLine2.widthAnchor.constraint(equalToConstant: 105),
+            
+            naverButton.topAnchor.constraint(equalTo: easyLoginView.bottomAnchor, constant: 20),
+            naverButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            naverButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            naverButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            signUpView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 80),
+            signUpView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -45),
+            
+            signUpButton.leadingAnchor.constraint(equalTo: signUpView.trailingAnchor),
+            signUpButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -80),
+            signUpButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -39),
         ])
     }
     
