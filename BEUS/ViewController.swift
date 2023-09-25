f4c0ef503db7b82c249d525c859e5cc837c432e1
timotheekim10 <timotheekim10@gmail.com>
2023-09-25 14:31:43 +0900

    Initial Commit

---
+++ BEUS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BEUS
+//
+//  Created by 티모시 킴 on 2023/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
