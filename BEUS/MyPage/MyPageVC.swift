c4e12793f07938d0c29f27f00f03de817dc706b5
timotheekim10 <timotheekim10@gmail.com>
2023-10-18 02:37:06 +0900

    design: 내 페이지 화면 그라데이션 백그라운드 추가

--- BEUS/MyPage/MyPageVC.swift
+++ BEUS/MyPage/MyPageVC.swift
@@ -8,11 +8,69 @@
 import UIKit
 
 class MyPageVC: UIViewController {
+    
+    let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.contentInsetAdjustmentBehavior = .never
+        return scrollView
+    }()
+    
+    let contentView: UIView = {
+        let contentView = UIView()
+        return contentView
+    }()
+    
+    let gradationBackground: UIImageView = {
+        let gradationBackground = UIImageView(image: UIImage(named: "GradationBackground"))
+        return gradationBackground
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         self.navigationController?.navigationBar.topItem?.title = "내 페이지"
+        setLayout()
     }
 
+}
+
+extension MyPageVC {
+    
+    func setLayout() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        
+        [gradationBackground].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            contentView.addSubview($0)
+        }
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+            contentView.heightAnchor.constraint(equalToConstant: 850),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            gradationBackground.topAnchor.constraint(equalTo: contentView.topAnchor),
+            gradationBackground.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            gradationBackground.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            gradationBackground.heightAnchor.constraint(equalToConstant: 200)
+        ])
+        
+    }
+    
 }
