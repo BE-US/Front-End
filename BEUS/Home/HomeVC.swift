9ec7108fe4756f0f5626da16bc6ae95399de8cf1
timotheekim10 <timotheekim10@gmail.com>
2023-10-17 16:47:08 +0900

    feat: 홈 화면 스크롤뷰 구현

--- BEUS/Home/HomeVC.swift
+++ BEUS/Home/HomeVC.swift
@@ -9,54 +9,79 @@ import UIKit
 
 class HomeVC: UIViewController {
     
-    let backgroundView: UIView = {
-        let backgroundView = UIView()
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.colors = [UIColor(hex: "#85D6A6").cgColor, UIColor(hex: "#FFE381").cgColor]
-        gradientLayer.locations = [0.0, 1.0]
-        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
-        gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
-        backgroundView.layer.insertSublayer(gradientLayer, at: 0)
-        return backgroundView
+    let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.contentInsetAdjustmentBehavior = .never
+        return scrollView
+    }()
+    
+    let contentView: UIView = {
+        let contentView = UIView()
+        return contentView
+    }()
+    
+    let gradationBackground: UIImageView = {
+        let gradationBackground = UIImageView(image: UIImage(named: "GradationBackground"))
+        return gradationBackground
+    }()
+    
+    let questionLabel: UILabel = {
+        let questionLabel = UILabel()
+        questionLabel.text = "어떤 분야의 프로가 되고 싶으신가요?"
+        questionLabel.textColor = #colorLiteral(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+        questionLabel.font = UIFont.boldSystemFont(ofSize: 17)
+        return questionLabel
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        let attributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
         self.navigationController?.navigationBar.topItem?.title = "홈"
-        self.navigationController?.navigationBar.titleTextAttributes = attributes
-        setUI()
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        backgroundView.layer.sublayers?.first?.frame = backgroundView.bounds
+        setLayout()
     }
     
 }
 
 extension HomeVC {
     
-    func setUI() {
-        makeSubView()
-        makeConstraint()
-    }
-    
-    func makeSubView() {
-        [backgroundView].forEach {
+    func setLayout() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        
+        [gradationBackground, questionLabel].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
-            view.addSubview($0)
+            contentView.addSubview($0)
         }
-        print(#function)
-    }
-    
-    func makeConstraint() {
+        
         NSLayoutConstraint.activate([
-            backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
-            backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            backgroundView.heightAnchor.constraint(equalToConstant: 200)
+            scrollView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+            contentView.heightAnchor.constraint(equalToConstant: 1000),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            gradationBackground.topAnchor.constraint(equalTo: contentView.topAnchor),
+            gradationBackground.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            gradationBackground.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            gradationBackground.heightAnchor.constraint(equalToConstant: 200)
+        ])
+        
+        NSLayoutConstraint.activate([
+            questionLabel.topAnchor.constraint(equalTo: gradationBackground.bottomAnchor, constant: 40),
+            questionLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 30)
         ])
     }
     
